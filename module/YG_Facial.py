983271483b85173b6100ba021bd848b6da2cdb0d
YGJeong-code <lupinx@naver.com>
2022-11-10 11:07:12 +0900

    22-11-09-수 ui 커넥트 작업중

--- module/YG_Facial.py
+++ module/YG_Facial.py
@@ -1,11 +1,18 @@
 import maya.cmds as cmds
+import Facial.module.YG_Facial_ARKit as ARKit
+reload(ARKit)
 
 #class
 class YG_Facial(object):
     def __init__(self):
         self.myWin = 'YG_Facial'
         self.size = 150
+
         self.createWindow()
+
+        self.myUpAxis = cmds.upAxis( q=True, axis=True )
+        if self.myUpAxis == 'z':
+            cmds.radioButton(self.upAxisZBtn, e=True, select=True)
 
     def createWindow(self):
         if cmds.window(self.myWin, ex=True):
@@ -14,11 +21,111 @@ class YG_Facial(object):
         self.myWin = cmds.window(self.myWin, t=self.myWin+'_v0.1', sizeable=True, resizeToFitChildren=True)
         self.myColor = {'red':[0.6,0,0],'orange':[0.6, 0.2, 0],'yellow':[0.7, 0.6, 0.1],'green':[0.4, 0.6, 0.1]}
 
-        ## build
+        ## up axis
         cmds.columnLayout( adjustableColumn=True, p=self.myWin )
-        self.targetFrame = cmds.frameLayout( label='Target', collapsable=True, collapse=False, bgc=self.myColor['red'], cc=self.winResize, ec=self.winResize)
+        self.upAxisFrame = cmds.frameLayout( label='Up Axis', collapsable=True, collapse=False, bgc=self.myColor['red'], cc=self.winResize, ec=self.winResize)
+        cmds.columnLayout( adjustableColumn=1, p=self.upAxisFrame )
+        cmds.rowLayout( numberOfColumns=2, columnWidth2=(self.size/2, self.size/2) )
+        cmds.radioCollection()
+        self.upAxisYBtn = cmds.radioButton( label='Y', select=True, cc=lambda *_:self.setUpAxis('y'))
+        self.upAxisZBtn = cmds.radioButton( label='Z', cc=lambda *_:self.setUpAxis('z'))
+        cmds.setParent( '..' )
+        cmds.setParent( '..' )
+
+        ## target
+        cmds.columnLayout( adjustableColumn=True, p=self.myWin )
+        self.targetFrame = cmds.frameLayout( label='Target', collapsable=True, collapse=False, bgc=self.myColor['orange'], cc=self.winResize, ec=self.winResize)
         cmds.columnLayout( adjustableColumn=1, p=self.targetFrame )
         cmds.button(label='make ARKit target', w=self.size*2, c=self.makeARKitTargetBtn)
+        cmds.setParent( '..' )
+        cmds.setParent( '..' )
+
+        ## pose
+        cmds.columnLayout( adjustableColumn=True, p=self.myWin )
+        self.poseFrame = cmds.frameLayout( label='Pose', collapsable=True, collapse=True, bgc=self.myColor['yellow'], cc=self.winResize, ec=self.winResize)
+        cmds.columnLayout( adjustableColumn=1, p=self.poseFrame )
+        cmds.button(label='0 Default', w=self.size*2, c=lambda *_:self.makePose('Default'))
+
+        cmds.text(l='eye', align='left')
+        cmds.rowColumnLayout(numberOfColumns=2, columnWidth=(self.size, self.size), adjustableColumn=1)
+        cmds.button(label='1 EyeBlinkLeft', w=self.size, c=lambda *_:self.makePose('EyeBlinkLeft'))
+        cmds.button(label='8 EyeBlinkRight', w=self.size, c=lambda *_:self.makePose('EyeBlinkRight'))
+
+        cmds.button(label='2 EyeLookDownLeft', w=self.size, c=lambda *_:self.makePose('EyeLookDownLeft'))
+        cmds.button(label='9 EyeLookDownRight', w=self.size, c=lambda *_:self.makePose('EyeLookDownRight'))
+
+        cmds.button(label='3 EyeLookInLeft', w=self.size, c=lambda *_:self.makePose('EyeLookInLeft'))
+        cmds.button(label='10 EyeLookInRight', w=self.size, c=lambda *_:self.makePose('EyeLookInRight'))
+
+        cmds.button(label='4 EyeLookOutLeft', w=self.size, c=lambda *_:self.makePose('EyeLookOutLeft'))
+        cmds.button(label='11 EyeLookOutRight', w=self.size, c=lambda *_:self.makePose('EyeLookOutRight'))
+
+        cmds.button(label='5 EyeLookUpLeft', w=self.size, c=lambda *_:self.makePose('EyeLookUpLeft'))
+        cmds.button(label='12 EyeLookUpRight', w=self.size, c=lambda *_:self.makePose('EyeLookUpRight'))
+
+        cmds.button(label='6 EyeSquintLeft', w=self.size, c=lambda *_:self.makePose('EyeSquintLeft'))
+        cmds.button(label='13 EyeSquintRight', w=self.size, c=lambda *_:self.makePose('EyeSquintRight'))
+
+        cmds.button(label='7 EyeWideLeft', w=self.size, c=lambda *_:self.makePose('EyeWideLeft'))
+        cmds.button(label='14 EyeWideRight', w=self.size, c=lambda *_:self.makePose('EyeWideRight'))
+
+        cmds.columnLayout( adjustableColumn=1, p=self.poseFrame )
+        cmds.text(l='jaw', align='left')
+        cmds.rowColumnLayout(numberOfColumns=2, columnWidth=(self.size, self.size), adjustableColumn=1)
+        cmds.button(label='16 JawLeft', w=self.size, c=lambda *_:self.makePose('JawLeft'))
+        cmds.button(label='17 JawRight', w=self.size, c=lambda *_:self.makePose('JawRight'))
+        cmds.button(label='15 JawForward', w=self.size, c=lambda *_:self.makePose('JawForward'))
+        cmds.button(label='18 JawOpen', w=self.size, c=lambda *_:self.makePose('JawOpen'))
+
+        cmds.columnLayout( adjustableColumn=1, p=self.poseFrame )
+        cmds.text(l='mouth', align='left')
+        cmds.rowColumnLayout(numberOfColumns=2, columnWidth=(self.size, self.size), adjustableColumn=1)
+        cmds.button(label='19 MouthFunnel', w=self.size, c=lambda *_:self.makePose('MouthFunnel'))
+        cmds.button(label='20 MouthPucker', w=self.size, c=lambda *_:self.makePose('MouthPucker'))
+        cmds.button(label='21 MouthLeft', w=self.size, c=lambda *_:self.makePose('MouthLeft'))
+        cmds.button(label='22 MouthRight', w=self.size, c=lambda *_:self.makePose('MouthRight'))
+        cmds.button(label='23 MouthSmileLeft', w=self.size, c=lambda *_:self.makePose('MouthSmileLeft'))
+        cmds.button(label='24 MouthSmileRight', w=self.size, c=lambda *_:self.makePose('MouthSmileRight'))
+        cmds.button(label='25 MouthFrownLeft', w=self.size, c=lambda *_:self.makePose('MouthFrownLeft'))
+        cmds.button(label='26 MouthFrownRight', w=self.size, c=lambda *_:self.makePose('MouthFrownRight'))
+        cmds.button(label='27 MouthDimpleLeft', w=self.size, c=lambda *_:self.makePose('MouthDimpleLeft'))
+        cmds.button(label='28 MouthDimpleRight', w=self.size, c=lambda *_:self.makePose('MouthDimpleRight'))
+        cmds.button(label='29 MouthStretchLeft', w=self.size, c=lambda *_:self.makePose('MouthStretchLeft'))
+        cmds.button(label='30 MouthStretchRight', w=self.size, c=lambda *_:self.makePose('MouthStretchRight'))
+        cmds.button(label='31 MouthRollLower', w=self.size, c=lambda *_:self.makePose('MouthRollLower'))
+        cmds.button(label='32 MouthRollUpper', w=self.size, c=lambda *_:self.makePose('MouthRollUpper'))
+        cmds.button(label='33 MouthShrugLower', w=self.size, c=lambda *_:self.makePose('MouthShrugLower'))
+        cmds.button(label='34 MouthShrugUpper', w=self.size, c=lambda *_:self.makePose('MouthShrugUpper'))
+        cmds.button(label='35 MouthPressLeft', w=self.size, c=lambda *_:self.makePose('MouthPressLeft'))
+        cmds.button(label='36 MouthPressRight', w=self.size, c=lambda *_:self.makePose('MouthPressRight'))
+        cmds.button(label='37 MouthLowerDownLeft', w=self.size, c=lambda *_:self.makePose('MouthLowerDownLeft'))
+        cmds.button(label='38 MouthLowerDownRight', w=self.size, c=lambda *_:self.makePose('MouthLowerDownRight'))
+        cmds.button(label='39 MouthUpperUpLeft', w=self.size, c=lambda *_:self.makePose('MouthUpperUpLeft'))
+        cmds.button(label='40 MouthUpperUpRight', w=self.size, c=lambda *_:self.makePose('MouthUpperUpRight'))
+        cmds.button(label='51 MouthClose', w=self.size, c=lambda *_:self.makePose('MouthClose'))
+
+        cmds.columnLayout( adjustableColumn=1, p=self.poseFrame )
+        cmds.text(l='brow', align='left')
+        cmds.rowColumnLayout(numberOfColumns=2, columnWidth=(self.size, self.size), adjustableColumn=1)
+        cmds.button(label='41 BrowDownLeft', w=self.size, c=lambda *_:self.makePose('BrowDownLeft'))
+        cmds.button(label='42 BrowDownRight', w=self.size, c=lambda *_:self.makePose('BrowDownRight'))
+        cmds.button(label='44 BrowOuterUpLeft', w=self.size, c=lambda *_:self.makePose('BrowOuterUpLeft'))
+        cmds.button(label='45 BrowOuterUpRight', w=self.size, c=lambda *_:self.makePose('BrowOuterUpRight'))
+        cmds.button(label='43 BrowInnerUp', w=self.size, c=lambda *_:self.makePose('BrowInnerUp'))
+
+        cmds.columnLayout( adjustableColumn=1, p=self.poseFrame )
+        cmds.text(l='cheek', align='left')
+        cmds.rowColumnLayout(numberOfColumns=2, columnWidth=(self.size, self.size), adjustableColumn=1)
+        cmds.button(label='47 CheekSquintLeft', w=self.size, c=lambda *_:self.makePose('CheekSquintLeft'))
+        cmds.button(label='48 CheekSquintRight', w=self.size, c=lambda *_:self.makePose('CheekSquintRight'))
+        cmds.button(label='46 CheekPuff', w=self.size, c=lambda *_:self.makePose('CheekPuff'))
+
+        cmds.columnLayout( adjustableColumn=1, p=self.poseFrame )
+        cmds.text(l='nose', align='left')
+        cmds.rowColumnLayout(numberOfColumns=2, columnWidth=(self.size, self.size), adjustableColumn=1)
+        cmds.button(label='49 NoseSneerLeft', w=self.size, c=lambda *_:self.makePose('NoseSneerLeft'))
+        cmds.button(label='50 NoseSneerRight', w=self.size, c=lambda *_:self.makePose('NoseSneerRight'))
+
         cmds.setParent( '..' )
         cmds.setParent( '..' )
 
@@ -29,7 +136,25 @@ class YG_Facial(object):
     # btn
     ############################################################################################################################################################################################################################
     def makeARKitTargetBtn(self, *args):
-        import Facial.module.YG_Facial_ARKit
+        ARKit.duplicateLODGroup()
+        ARKit.importUI()
+
+    def makePose(self, target):
+        ARKit.makePose(target)
+
+    def setUpAxis(self, axis):
+        cmds.upAxis( ax=axis, rv=True )
+
+        if axis == 'y':
+            if bool(cmds.ls('root_drv')):
+                cmds.setAttr('root_drv.rx', -90)
+                cmds.setAttr('headRig_grp.rx', -90)
+                cmds.setAttr('Lights.rx', -90)
+        else:
+            if bool(cmds.ls('root_drv')):
+                cmds.setAttr('root_drv.rx', 0)
+                cmds.setAttr('headRig_grp.rx', 0)
+                cmds.setAttr('Lights.rx', 0)
 
     ############################################################################################################################################################################################################################
     # window resize
@@ -41,5 +166,5 @@ class YG_Facial(object):
         size = cmds.window(self.myWin, q=True, widthHeight=True)
         print size
 
-myTest = YG_Facial()
-myTest.winResize()
+myClass = YG_Facial()
+myClass.winResize()
--- module/YG_Facial_ARKit.py
+++ module/YG_Facial_ARKit.py
@@ -1,5 +1,8 @@
 import maya.cmds as cmds
+import Facial.module.YG_Facial_ARKitDict as ARKitDict
+reload(ARKitDict)
 
+# old
 def fwdBackKey(myTime, myCon, myPos, myValue):
     cmds.currentTime( myTime-1, edit=True )
     cmds.setKeyframe(myCon, attribute=myPos, v=0)
@@ -24,248 +27,301 @@ def backKey(myTime, myCon, myPos, myValue):
     cmds.currentTime( myTime+1, edit=True )
     cmds.setKeyframe(myCon, attribute=myPos, v=0)
 
-cmds.currentUnit( time='film' )
-cmds.playbackOptions( e=True, min=0, max=51, aet=51 )
+def makeKey():
+    cmds.currentUnit( time='film' )
+    cmds.playbackOptions( e=True, min=0, max=51, aet=51 )
 
+    # 0 Default
 
-# 0 Default
+    # 1 EyeBlinkLeft
+    fwdBackKey(1, 'CTRL_L_eye_blink','ty',1)
 
-# 1 EyeBlinkLeft
-fwdBackKey(1, 'CTRL_L_eye_blink','translateY',1)
+    # 2 EyeLookDownLeft
+    fwdBackKey(2, 'CTRL_L_eye','ty',-1)
 
-# 2 EyeLookDownLeft
-fwdBackKey(2, 'CTRL_L_eye','translateY',-1)
+    # 3 EyeLookInLeft
+    fwdKey(3, 'CTRL_L_eye','tx',-1)
 
-# 3 EyeLookInLeft
-fwdKey(3, 'CTRL_L_eye','translateX',-1)
+    # 4 EyeLookOutLeft
+    backKey(4, 'CTRL_L_eye','tx',1)
 
-# 4 EyeLookOutLeft
-backKey(4, 'CTRL_L_eye','translateX',1)
+    # 5 EyeLookUpLeft
+    fwdBackKey(5, 'CTRL_L_eye','ty',1)
 
-# 5 EyeLookUpLeft
-fwdBackKey(5, 'CTRL_L_eye','translateY',1)
+    # 6 EyeSquintLeft
+    fwdBackKey(6, 'CTRL_L_eye_squintInner','ty',1)
 
-# 6 EyeSquintLeft
-fwdBackKey(6, 'CTRL_L_eye_squintInner','translateY',1)
+    # 7 EyeWideLeft
+    fwdBackKey(7, 'CTRL_L_eye_blink','ty',-1)
 
-# 7 EyeWideLeft
-fwdBackKey(7, 'CTRL_L_eye_blink','translateY',-1)
+    # 8 EyeBlinkRight
+    fwdBackKey(8, 'CTRL_R_eye_blink','ty',1)
 
-# 8 EyeBlinkRight
-fwdBackKey(8, 'CTRL_R_eye_blink','translateY',1)
+    # 9 EyeLookDownRight
+    fwdBackKey(9, 'CTRL_R_eye','ty', -1)
 
-# 9 EyeLookDownRight
-fwdBackKey(9, 'CTRL_R_eye','translateY', -1)
+    # 10 EyeLookInRight
+    fwdKey(10, 'CTRL_R_eye','tx', 1)
 
-# 10 EyeLookInRight
-fwdKey(10, 'CTRL_R_eye','translateX', 1)
+    # 11 EyeLookOutRight
+    backKey(11, 'CTRL_R_eye','tx', -1)
 
-# 11 EyeLookOutRight
-backKey(11, 'CTRL_R_eye','translateX', -1)
+    # 12 EyeLookUpRight
+    fwdBackKey(12, 'CTRL_R_eye','ty', 1)
 
-# 12 EyeLookUpRight
-fwdBackKey(12, 'CTRL_R_eye','translateY', 1)
+    # 13 EyeSquintRight
+    fwdBackKey(13, 'CTRL_R_eye_squintInner','ty', 1)
 
-# 13 EyeSquintRight
-fwdBackKey(13, 'CTRL_R_eye_squintInner','translateY', 1)
+    # 14 EyeWideRight
+    fwdBackKey(14, 'CTRL_R_eye_blink','ty', -1)
 
-# 14 EyeWideRight
-fwdBackKey(14, 'CTRL_R_eye_blink','translateY', -1)
+    # 15 JawForward
+    fwdBackKey(15, 'CTRL_C_jaw_fwdBack','ty', -1)
 
-# 15 JawForward
-fwdBackKey(15, 'CTRL_C_jaw_fwdBack','translateY', -1)
+    # 16 JawLeft
+    fwdKey(16, 'CTRL_C_jaw','tx', -1)
 
-# 16 JawLeft
-fwdKey(16, 'CTRL_C_jaw','translateX', -1)
+    # 17 JawRight
+    backKey(17, 'CTRL_C_jaw','tx', 1)
 
-# 17 JawRight
-backKey(17, 'CTRL_C_jaw','translateX', 1)
+    # 18 JawOpen
+    fwdBackKey(18, 'CTRL_C_jaw','ty', 1)
 
-# 18 JawOpen
-fwdBackKey(18, 'CTRL_C_jaw','translateY', 1)
+    # 19 MouthFunnel
+    fwdKey(19, 'CTRL_L_mouth_funnelU','ty', 1)
+    fwdKey(19, 'CTRL_R_mouth_funnelU','ty', 1)
+    fwdKey(19, 'CTRL_L_mouth_funnelD','ty', 1)
+    fwdKey(19, 'CTRL_R_mouth_funnelD','ty', 1)
 
-# 19 MouthFunnel
-fwdKey(19, 'CTRL_L_mouth_funnelU','translateY', 1)
-fwdKey(19, 'CTRL_R_mouth_funnelU','translateY', 1)
-fwdKey(19, 'CTRL_L_mouth_funnelD','translateY', 1)
-fwdKey(19, 'CTRL_R_mouth_funnelD','translateY', 1)
+    # 20 MouthPucker
+    fwdBackKey(20, 'CTRL_L_mouth_purseU','ty', 1)
+    fwdBackKey(20, 'CTRL_R_mouth_purseU','ty', 1)
+    fwdBackKey(20, 'CTRL_L_mouth_purseD','ty', 1)
+    fwdBackKey(20, 'CTRL_R_mouth_purseD','ty', 1)
+    backKey(20, 'CTRL_L_mouth_funnelU','ty', 0.75)
+    backKey(20, 'CTRL_R_mouth_funnelU','ty', 0.75)
+    backKey(20, 'CTRL_L_mouth_funnelD','ty', 0.75)
+    backKey(20, 'CTRL_R_mouth_funnelD','ty', 0.75)
+    fwdBackKey(20, 'CTRL_L_mouth_towardsU','ty', 0.41)
+    fwdBackKey(20, 'CTRL_R_mouth_towardsU','ty', 0.41)
+    fwdBackKey(20, 'CTRL_L_mouth_towardsD','ty', 0.41)
+    fwdBackKey(20, 'CTRL_R_mouth_towardsD','ty', 0.41)
 
-# 20 MouthPucker
-fwdBackKey(20, 'CTRL_L_mouth_purseU','translateY', 1)
-fwdBackKey(20, 'CTRL_R_mouth_purseU','translateY', 1)
-fwdBackKey(20, 'CTRL_L_mouth_purseD','translateY', 1)
-fwdBackKey(20, 'CTRL_R_mouth_purseD','translateY', 1)
-backKey(20, 'CTRL_L_mouth_funnelU','translateY', 0.75)
-backKey(20, 'CTRL_R_mouth_funnelU','translateY', 0.75)
-backKey(20, 'CTRL_L_mouth_funnelD','translateY', 0.75)
-backKey(20, 'CTRL_R_mouth_funnelD','translateY', 0.75)
-fwdBackKey(20, 'CTRL_L_mouth_towardsU','translateY', 0.41)
-fwdBackKey(20, 'CTRL_R_mouth_towardsU','translateY', 0.41)
-fwdBackKey(20, 'CTRL_L_mouth_towardsD','translateY', 0.41)
-fwdBackKey(20, 'CTRL_R_mouth_towardsD','translateY', 0.41)
+    # 21 MouthLeft
+    fwdKey(21, 'CTRL_C_mouth','tx', 1)
 
-# 21 MouthLeft
-fwdKey(21, 'CTRL_C_mouth','translateX', 1)
+    # 22 MouthRight
+    backKey(22, 'CTRL_C_mouth','tx', -1)
 
-# 22 MouthRight
-backKey(22, 'CTRL_C_mouth','translateX', -1)
+    # 23 MouthSmileLeft
+    fwdBackKey(23, 'CTRL_L_mouth_cornerPull','ty', 1)
 
-# 23 MouthSmileLeft
-fwdBackKey(23, 'CTRL_L_mouth_cornerPull','translateY', 1)
+    # 24 MouthSmileRight
+    fwdBackKey(24, 'CTRL_R_mouth_cornerPull','ty', 1)
 
-# 24 MouthSmileRight
-fwdBackKey(24, 'CTRL_R_mouth_cornerPull','translateY', 1)
+    # 25 MouthFrownLeft
+    fwdBackKey(25, 'CTRL_L_mouth_cornerDepress','ty', 1)
 
-# 25 MouthFrownLeft
-fwdBackKey(25, 'CTRL_L_mouth_cornerDepress','translateY', 1)
+    # 26 MouthFrownRight
+    fwdBackKey(26, 'CTRL_R_mouth_cornerDepress','ty', 1)
 
-# 26 MouthFrownRight
-fwdBackKey(26, 'CTRL_R_mouth_cornerDepress','translateY', 1)
+    # 27 MouthDimpleLeft
+    fwdBackKey(27, 'CTRL_L_mouth_dimple','ty', 1)
 
-# 27 MouthDimpleLeft
-fwdBackKey(27, 'CTRL_L_mouth_dimple','translateY', 1)
+    # 28 MouthDimpleRight
+    fwdBackKey(28, 'CTRL_R_mouth_dimple','ty', 1)
 
-# 28 MouthDimpleRight
-fwdBackKey(28, 'CTRL_R_mouth_dimple','translateY', 1)
+    # 29 MouthStretchLeft
+    fwdBackKey(29, 'CTRL_L_mouth_stretch','ty', 1)
 
-# 29 MouthStretchLeft
-fwdBackKey(29, 'CTRL_L_mouth_stretch','translateY', 1)
+    # 30 MouthStretchRight
+    fwdBackKey(30, 'CTRL_R_mouth_stretch','ty', 1)
+
+    # 31 MouthRollLower
+    fwdKey(31, 'CTRL_L_mouth_lipsRollU','ty', 0.5)
+    fwdKey(31, 'CTRL_R_mouth_lipsRollU','ty', 0.5)
+    fwdBackKey(31, 'CTRL_L_mouth_lipsRollD','ty', 1)
+    fwdBackKey(31, 'CTRL_R_mouth_lipsRollD','ty', 1)
 
-# 30 MouthStretchRight
-fwdBackKey(30, 'CTRL_R_mouth_stretch','translateY', 1)
+    # 32 MouthRollUpper
+    backKey(32, 'CTRL_L_mouth_lipsRollU','ty', 1)
+    backKey(32, 'CTRL_R_mouth_lipsRollU','ty', 1)
 
-# 31 MouthRollLower
-fwdKey(31, 'CTRL_L_mouth_lipsRollU','translateY', 0.5)
-fwdKey(31, 'CTRL_R_mouth_lipsRollU','translateY', 0.5)
-fwdBackKey(31, 'CTRL_L_mouth_lipsRollD','translateY', 1)
-fwdBackKey(31, 'CTRL_R_mouth_lipsRollD','translateY', 1)
-
-# 32 MouthRollUpper
-backKey(32, 'CTRL_L_mouth_lipsRollU','translateY', 1)
-backKey(32, 'CTRL_R_mouth_lipsRollU','translateY', 1)
-
-# 33 MouthShrugLower
-fwdBackKey(33, 'CTRL_L_jaw_ChinRaiseD','translateY', 1)
-fwdBackKey(33, 'CTRL_R_jaw_ChinRaiseD','translateY', 1)
-
-# 34 MouthShrugUpper
-fwdBackKey(34, 'CTRL_L_jaw_ChinRaiseU','translateY', 1)
-fwdBackKey(34, 'CTRL_R_jaw_ChinRaiseU','translateY', 1)
-
-# 35 MouthPressLeft
-fwdBackKey(35, 'CTRL_L_mouth_pressU','translateY', 1)
-fwdBackKey(35, 'CTRL_L_mouth_pressD','translateY', 1)
-
-# 36 MouthPressRight
-fwdBackKey(36, 'CTRL_R_mouth_pressU','translateY', 1)
-fwdBackKey(36, 'CTRL_R_mouth_pressD','translateY', 1)
-
-# 37 MouthLowerDownLeft
-fwdBackKey(37, 'CTRL_L_mouth_lowerLipDepress','translateY', 1)
-
-# 38 MouthLowerDownRight
-fwdBackKey(38, 'CTRL_R_mouth_lowerLipDepress','translateY', 1)
-
-# 39 MouthUpperUpLeft
-fwdBackKey(39, 'CTRL_L_mouth_upperLipRaise','translateY', 1)
-
-# 40 MouthUpperUpRight
-fwdBackKey(40, 'CTRL_R_mouth_upperLipRaise','translateY', 1)
-
-# 41 BrowDownLeft
-fwdBackKey(41, 'CTRL_L_brow_down','translateY', 1)
-
-# 42 BrowDownRight
-fwdBackKey(42, 'CTRL_R_brow_down','translateY', 1)
-
-# 43 BrowInnerUp
-fwdKey(43, 'CTRL_L_brow_raiseIn','translateY', 1)
-fwdBackKey(43, 'CTRL_R_brow_raiseIn','translateY', 1)
-fwdBackKey(43, 'CTRL_L_brow_lateral','translateY', 1)
-fwdBackKey(43, 'CTRL_R_brow_lateral','translateY', 1)
-
-# 44 BrowOuterUpLeft
-backKey(44, 'CTRL_L_brow_raiseIn','translateY', 1)
-fwdBackKey(44, 'CTRL_L_brow_raiseOut','translateY', 1)
-
-# 45 BrowOuterUpRight
-fwdBackKey(45, 'CTRL_R_brow_raiseIn','translateY', 1)
-fwdBackKey(45, 'CTRL_R_brow_raiseOut','translateY', 1)
-
-# 46 CheekPuff
-fwdBackKey(46, 'CTRL_L_mouth_suckBlow','translateY', 1)
-fwdBackKey(46, 'CTRL_R_mouth_suckBlow','translateY', 1)
-fwdBackKey(46, 'CTRL_L_mouth_lipsBlow','translateY', 1)
-fwdBackKey(46, 'CTRL_R_mouth_lipsBlow','translateY', 1)
-
-# 47 CheekSquintLeft
-fwdBackKey(47, 'CTRL_L_eye_cheekRaise','translateY', 1)
-
-# 48 CheekSquintRight
-fwdBackKey(48, 'CTRL_R_eye_cheekRaise','translateY', 1)
-
-# 49 NoseSneerLeft
-fwdBackKey(49, 'CTRL_L_nose','translateY', 1)
-
-# 50 NoseSneerRight
-fwdBackKey(50, 'CTRL_R_nose','translateY', 1)
-
-# 51 TonqueOut
-# fwdBackKey(51, 'CTRL_C_tongue','translateY', -0.82)
-# fwdBackKey(51, 'CTRL_C_tongue_inOut','translateY', -0.76)
-# fwdBackKey(51, 'CTRL_C_tongue_roll','translateY', -0.24)
-# fwdBackKey(51, 'CTRL_C_tongue_narrowWide','translateY', 0.62)
-
-# 51 mouthClose
-fwdBackKey(51, 'CTRL_C_jaw','translateY', 1)
-fwdBackKey(51, 'CTRL_L_mouth_lipsTogetherU','translateY', 1)
-fwdBackKey(51, 'CTRL_R_mouth_lipsTogetherU','translateY', 1)
-fwdBackKey(51, 'CTRL_L_mouth_lipsTogetherD','translateY', 1)
-fwdBackKey(51, 'CTRL_R_mouth_lipsTogetherD','translateY', 1)
-
-# duplicate LOD group
-
-# myLod = 'head_lod0_grp'
-myList = cmds.listRelatives('head_lod0_grp', c=True)
-myMesh = cmds.polyUniteSkinned( myList, ch=0 )
-
-myGrp = cmds.group(em=True, n='target_grp')
-myList = ['Default','EyeBlinkLeft','EyeLookDownLeft','EyeLookInLeft','EyeLookOutLeft',
-        'EyeLookUpLeft','EyeSquintLeft','EyeWideLeft','EyeBlinkRight','EyeLookDownRight',
-        'EyeLookInRight','EyeLookOutRight','EyeLookUpRight','EyeSquintRight','EyeWideRight',
-        'JawForward','JawLeft','JawRight','JawOpen','MouthFunnel',
-        'MouthPucker','MouthLeft','MouthRight','MouthSmileLeft','MouthSmileRight',
-        'MouthFrownLeft','MouthFrownRight','MouthDimpleLeft','MouthDimpleRight','MouthStretchLeft',
-        'MouthStretchRight','MouthRollLower','MouthRollUpper','MouthShrugLower','MouthShrugUpper',
-        'MouthPressLeft','MouthPressRight','MouthLowerDownLeft','MouthLowerDownRight','MouthUpperUpLeft',
-        'MouthUpperUpRight','BrowDownLeft','BrowDownRight','BrowInnerUp','BrowOuterUpLeft',
-        'BrowOuterUpRight','CheekPuff','CheekSquintLeft','CheekSquintRight','NoseSneerLeft',
-        'NoseSneerRight']
-
-for i in range(len(myList)):
-    #print myList[i]
-    cmds.currentTime( i, edit=True )
-    cmds.duplicate(myMesh, n=myList[i], rc=True)
-    cmds.parent(myList[i], myGrp)
-
-# mouthClose target
-cmds.currentTime( 0, edit=True )
-myHead = cmds.duplicate(myMesh)
-
-cmds.currentTime( 51, edit=True )
-target1 = cmds.duplicate(myMesh)
-
-cmds.currentTime( 18, edit=True )
-target2 = cmds.duplicate(myMesh)
-
-myBlend = cmds.blendShape( target1, target2, myHead )
-cmds.setAttr(myBlend[0]+'.'+target1[0], 1)
-cmds.setAttr(myBlend[0]+'.'+target2[0], -1)
-
-myTemp = cmds.group(em=True, n='MouthClose_grp')
-cmds.parent(myTemp,myGrp)
-myTarget = cmds.duplicate(myHead, rc=True)
-cmds.parent(myTarget[0], myTemp)
-cmds.delete(myHead,target1,target2)
-
-cmds.currentTime( 0, edit=True )
+    # 33 MouthShrugLower
+    fwdBackKey(33, 'CTRL_L_jaw_ChinRaiseD','ty', 1)
+    fwdBackKey(33, 'CTRL_R_jaw_ChinRaiseD','ty', 1)
+
+    # 34 MouthShrugUpper
+    fwdBackKey(34, 'CTRL_L_jaw_ChinRaiseU','ty', 1)
+    fwdBackKey(34, 'CTRL_R_jaw_ChinRaiseU','ty', 1)
+
+    # 35 MouthPressLeft
+    fwdBackKey(35, 'CTRL_L_mouth_pressU','ty', 1)
+    fwdBackKey(35, 'CTRL_L_mouth_pressD','ty', 1)
+
+    # 36 MouthPressRight
+    fwdBackKey(36, 'CTRL_R_mouth_pressU','ty', 1)
+    fwdBackKey(36, 'CTRL_R_mouth_pressD','ty', 1)
+
+    # 37 MouthLowerDownLeft
+    fwdBackKey(37, 'CTRL_L_mouth_lowerLipDepress','ty', 1)
+
+    # 38 MouthLowerDownRight
+    fwdBackKey(38, 'CTRL_R_mouth_lowerLipDepress','ty', 1)
+
+    # 39 MouthUpperUpLeft
+    fwdBackKey(39, 'CTRL_L_mouth_upperLipRaise','ty', 1)
+
+    # 40 MouthUpperUpRight
+    fwdBackKey(40, 'CTRL_R_mouth_upperLipRaise','ty', 1)
+
+    # 41 BrowDownLeft
+    fwdBackKey(41, 'CTRL_L_brow_down','ty', 1)
+
+    # 42 BrowDownRight
+    fwdBackKey(42, 'CTRL_R_brow_down','ty', 1)
+
+    # 43 BrowInnerUp
+    fwdKey(43, 'CTRL_L_brow_raiseIn','ty', 1)
+    fwdBackKey(43, 'CTRL_R_brow_raiseIn','ty', 1)
+    fwdBackKey(43, 'CTRL_L_brow_lateral','ty', 1)
+    fwdBackKey(43, 'CTRL_R_brow_lateral','ty', 1)
+
+    # 44 BrowOuterUpLeft
+    backKey(44, 'CTRL_L_brow_raiseIn','ty', 1)
+    fwdBackKey(44, 'CTRL_L_brow_raiseOut','ty', 1)
+
+    # 45 BrowOuterUpRight
+    fwdBackKey(45, 'CTRL_R_brow_raiseIn','ty', 1)
+    fwdBackKey(45, 'CTRL_R_brow_raiseOut','ty', 1)
+
+    # 46 CheekPuff
+    fwdBackKey(46, 'CTRL_L_mouth_suckBlow','ty', 1)
+    fwdBackKey(46, 'CTRL_R_mouth_suckBlow','ty', 1)
+    fwdBackKey(46, 'CTRL_L_mouth_lipsBlow','ty', 1)
+    fwdBackKey(46, 'CTRL_R_mouth_lipsBlow','ty', 1)
+
+    # 47 CheekSquintLeft
+    fwdBackKey(47, 'CTRL_L_eye_cheekRaise','ty', 1)
+
+    # 48 CheekSquintRight
+    fwdBackKey(48, 'CTRL_R_eye_cheekRaise','ty', 1)
+
+    # 49 NoseSneerLeft
+    fwdBackKey(49, 'CTRL_L_nose','ty', 1)
+
+    # 50 NoseSneerRight
+    fwdBackKey(50, 'CTRL_R_nose','ty', 1)
+
+    # 51 TonqueOut
+    # fwdBackKey(51, 'CTRL_C_tongue','ty', -0.82)
+    # fwdBackKey(51, 'CTRL_C_tongue_inOut','ty', -0.76)
+    # fwdBackKey(51, 'CTRL_C_tongue_roll','ty', -0.24)
+    # fwdBackKey(51, 'CTRL_C_tongue_narrowWide','ty', 0.62)
+
+    # 51 mouthClose
+    fwdBackKey(51, 'CTRL_C_jaw','ty', 1)
+    fwdBackKey(51, 'CTRL_L_mouth_lipsTogetherU','ty', 1)
+    fwdBackKey(51, 'CTRL_R_mouth_lipsTogetherU','ty', 1)
+    fwdBackKey(51, 'CTRL_L_mouth_lipsTogetherD','ty', 1)
+    fwdBackKey(51, 'CTRL_R_mouth_lipsTogetherD','ty', 1)
+
+# new
+def duplicateLODGroup():
+    # lod0
+    myList = cmds.listRelatives('head_lod0_grp', c=True)
+    myMesh = cmds.polyUniteSkinned( myList, ch=0 )
+    myMesh = myMesh[0]
+
+    # target group
+    myGrp = cmds.group(em=True, n='target_grp')
+    cmds.hide(myGrp)
+
+    # make pose & duplicate
+    for target in ARKitDict.myList:
+        makePose(target)
+        cmds.duplicate(myMesh, n=target, rc=True)
+
+        if target != 'Default':
+            cmds.parent(target, myGrp)
+
+    # mouth close
+    makePose('Default')
+    myHead = cmds.duplicate(myMesh)
+
+    makePose('MouthClose')
+    target1 = cmds.duplicate(myMesh)
+
+    makePose('JawOpen')
+    target2 = cmds.duplicate(myMesh)
+
+    myBlend = cmds.blendShape( target1, target2, myHead )
+    cmds.setAttr(myBlend[0]+'.'+target1[0], 1)
+    cmds.setAttr(myBlend[0]+'.'+target2[0], -1)
+
+    myTarget = cmds.duplicate(myHead, n='MouthClose', rc=True)
+    cmds.parent(myTarget[0], myGrp)
+    cmds.delete(myHead,target1,target2)
+
+    # blendShape
+    myTargetList = cmds.listRelatives(myGrp, c=True)
+    cmds.blendShape( myTargetList, 'Default', n='BS_ARKit' )
+    cmds.delete(myGrp)
+    # cmds.hide(myMesh)
+
+    makePose('Default')
+
+    # unlock
+    myList = ['tx','ty','tz','rx','ry','rz','sx','sy','sz']
+    for i in myList:
+        cmds.setAttr('%s.%s' % ('Default', i), keyable=True, lock=False)
+
+    # connect blendShape 2 ui
+
+def uiCon(con):
+    cmds.ls(con)
+    for i in cmds.ls(con):
+        if 'head_grp' not in cmds.listRelatives(i, f=True)[0]:
+            return i
+
+def defaultPose():
+    for item in ARKitDict.myDict:
+        for con in ARKitDict.myDict[item]:
+            for axis in ARKitDict.myDict[item][con]:
+                if len(cmds.ls(con))>1:
+                    for i in cmds.ls(con):
+                        cmds.setAttr(i+'.'+axis, 0)
+                else:
+                    cmds.setAttr(con+'.'+axis, 0)
+
+def makePose(target):
+    if target == 'Default':
+        defaultPose()
+    else:
+        defaultPose()
+
+        for con in ARKitDict.myDict[target]:
+            for axis in ARKitDict.myDict[target][con]:
+                value = ARKitDict.myDict[target][con][axis]
+
+                if len(cmds.ls(con))>1:
+                    for i in cmds.ls(con):
+                        cmds.setAttr(i+'.'+axis, value)
+                else:
+                    cmds.setAttr(con+'.'+axis, value)
+
+def importUI():
+    usd = cmds.internalVar(usd=True)
+    mayascripts = '%s/%s' % (usd.rsplit('/', 3)[0], 'scripts/')
+    tempPath = mayascripts + "Facial/template/facial_ui_v01.ma"
+
+    cmds.file(tempPath, i=True)
+
+def uiCon(con):
+    cmds.ls(con)
+    for i in cmds.ls(con):
+        if 'head_grp' not in cmds.listRelatives(i, f=True)[0]:
+            return i
+
+def connectBlendShape2UI():
+    cmds.connectAttr(uiCon('CTRL_L_eye_blink')+'.ty', 'BS_ARKit.EyeBlinkLeft', f=True)
